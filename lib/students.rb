73d374fb30042809afbe9edb21eb42b5e6b32961
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-26 01:42:23 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   "select max(gpa) from students"
@@ -17,5 +17,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  "select avg(gpa) from student where grade = 9" 
+  "select avg(gpa) from students where grade = 9"
 end
